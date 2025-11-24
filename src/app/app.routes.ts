2899f3c3d3a3645e1b351fddb04f8eb10495e3db
Jhonatam Soares <jhonatamtecnologias@gmail.com>
2025-11-23 21:40:08 -0300

    Atualiza README com documentação profissional

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CatalogoComponent } from './pages/catalogo/catalogo';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: CatalogoComponent
+  }
+];
