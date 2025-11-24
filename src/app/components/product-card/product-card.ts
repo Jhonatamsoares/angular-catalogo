a557c06ef55ee680d0069b0ea09ac1522acb0427
Jhonatam Soares <jhonatamtecnologias@gmail.com>
2025-11-23 21:10:00 -0300

    feat: adiciona component product-card

---
+++ src/app/components/product-card/product-card.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-card.html',
+  styleUrls: ['./product-card.css']
+})
+export class ProductCardComponent {
+  @Input() produto: any;
+}
